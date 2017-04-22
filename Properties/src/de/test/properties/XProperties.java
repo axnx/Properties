c4755c31307f608e17573d9fc3ee185fa02eab28
storost@web.de <storost@web.de>
2017-04-22 21:54:34 +0200

    + XProperties.java

---
+++ Properties/src/de/test/properties/XProperties.java
@@ -0,0 +1,41 @@
+package de.test.properties;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class XProperties {
+  
+	public Properties read (String propertiesFile) {
+	
+		Properties prop = new Properties();
+		InputStream input = null;
+		
+		try {
+
+			input = new FileInputStream(propertiesFile);
+			prop.load(input);
+			if(prop.containsKey("externalfile")){
+				String externalfile = prop.getProperty("externalfile");
+				if(prop.getProperty("externalfile").length() > 0 ){
+					return this.read(externalfile);
+				}
+			}
+			
+			return prop;
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return null;
+  }
+}
